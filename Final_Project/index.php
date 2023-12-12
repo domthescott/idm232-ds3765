7dfb50e3ccb7e0c19e385f6f728d474e98560a22
domthescott <114445816+domthescott@users.noreply.github.com>
2023-12-11 23:23:38 -0500

    Case Study

---
+++ Final_Project/index.php
@@ -0,0 +1,21 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Final_Project</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Gabarito&display=swap" rel="stylesheet">
+</head>
+<body>
+
+
+<?php
+echo "Hello, PHP is working!";
+?>
+
+
+</body>
+</html>
